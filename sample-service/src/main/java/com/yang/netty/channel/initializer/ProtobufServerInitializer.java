1b49903e0f6caf32e799d3a83832403cc0cb1388
yangyaming <yangyaming@jd.com>
2018-08-16 09:15:04 +0800

    protobuf

--- sample-service/src/main/java/com/yang/netty/channel/initializer/ProtobufServerInitializer.java
+++ sample-service/src/main/java/com/yang/netty/channel/initializer/ProtobufServerInitializer.java
@@ -11,7 +11,11 @@ import com.yang.netty.protobuf.SubscribeReqProto;
 import com.yang.netty.server.handler.ProtobufServerHandler;
 
 /**
- * 无解码器的Handler
+ * ProtobufDecoder仅仅只能解码，但是不能处理半包的问题，
+ * 所以在ProtobufDecoder之前有个ProtobufVarint32FrameDecoder
+ * ProtobufVarint32FrameDecoder可以处理半包消息,
+ * 如果你注释掉这个解码器，运行程序会出错
+ * 当然你也可以继承ByteToMessageDecoder,自己处理半包消息
  * @author yangyaming
  */
 public class ProtobufServerInitializer extends ChannelInitializer<SocketChannel>{
